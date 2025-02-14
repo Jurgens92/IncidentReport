de037bc2e053f7b500f545cc8bf406c4c4b68c60
JurgensSteyn <steynjurgens@gmail.com>
2025-02-14 10:19:11 +0200

    Update: IP Address Logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = 'your-secret-key-here'
@@ -10,5 +11,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+
+# Updated ProxyFix configuration
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, 
+    x_for=1,      # Number of proxy servers
+    x_proto=1,    # Number of proxies that set X-Forwarded-Proto
+    x_host=1      # Number of proxies that set X-Forwarded-Host
+)
 
 from app import routes, models
